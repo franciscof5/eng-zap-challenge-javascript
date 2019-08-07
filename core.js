a31c60bda25f48846ebec6de02c07f5de4bf504b
Francisco Mat <francisco@f5sites.com>
2019-08-06 23:46:10 -0300

    Axios receive promise from AWS zap server

--- core.js
+++ core.js
@@ -1,6 +1,13 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
+	el: '#app',
+	data () {
+		return {
+			message: 'Loagin data from zap servers...'
+		}
+	},
+	mounted () {
+		axios
+		.get('http://grupozap-code-challenge.s3-website-us-east-1.amazonaws.com/sources/source-1.json')
+		.then(response => (this.message = response))
+	}
 })
