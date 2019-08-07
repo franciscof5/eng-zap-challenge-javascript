0744879009f49493502b59382d5c89d149daeaf8
Francisco Mat <francisco@f5sites.com>
2019-08-07 00:17:35 -0300

    working with returned data

--- core.js
+++ core.js
@@ -2,12 +2,23 @@ var app = new Vue({
 	el: '#app',
 	data () {
 		return {
-			message: 'Loagin data from zap servers...'
+			tipo: null,
+			loading: true,
+			errored: false,
+			data: 'Loagin data from zap servers...',
 		}
 	},
 	mounted () {
 		axios
 		.get('http://grupozap-code-challenge.s3-website-us-east-1.amazonaws.com/sources/source-1.json')
-		.then(response => (this.message = response))
+		.then(response => {
+			this.loading = false;
+			this.data = response.data
+		}).catch(error => {
+			console.log(error)
+			this.errored = true
+		})/*.finally(() => {
+			this.loading = false
+		})*/
 	}
 })
