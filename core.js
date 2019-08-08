86b57ae8b833f165a188a53395faa3d19715f156
Francisco Mat <francisco@f5sites.com>
2019-08-07 22:30:27 -0300

    filter array logic level 1

--- core.js
+++ core.js
@@ -6,6 +6,8 @@ var app = new Vue({
 			loading: true,
 			errored: false,
 			data: 'Loagin data from zap servers...',
+			dataZap: null,
+			dataViva: null,
 		}
 	},
 	mounted () {
@@ -13,7 +15,18 @@ var app = new Vue({
 		.get('http://grupozap-code-challenge.s3-website-us-east-1.amazonaws.com/sources/source-1.json')
 		.then(response => {
 			this.loading = false;
-			this.data = response.data
+			
+			this.data = response.data;
+			
+			this.dataViva = response.data.filter(function(item) {
+				if(item.usableAreas>100 && item.pricingInfos.businessType == "RENTAL")
+				return item;
+			});
+
+			this.dataZap = response.data.filter(function(item) {
+				return item.usableAreas<100;
+			});
+
 		}).catch(error => {
 			console.log(error)
 			this.errored = true
